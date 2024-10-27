8dfdeb8df9b72d4ef157ca3c5f68f6cc687a7326
Morgan Arrington <mjarring96@gmail.com>
2024-10-27 08:32:30 -0400

    Modify unit tests to use files as input

--- src/Code/CalorieParserTest.cpp
+++ src/Code/CalorieParserTest.cpp
@@ -1,17 +1,31 @@
 #include "CalorieParser.hpp"
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-const std::string input =
-    "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n";
+const std::string testFilePath = "input/day/1/test";
 
-TEST_CASE("Testing readCalories") {
+std::string openTestFile(const std::string &aPath) {
+  std::ifstream inputFile(aPath);
+  assert(inputFile.is_open());
+
+  std::stringstream buffer;
+  buffer << inputFile.rdbuf();
+  return buffer.str();
+}
+
+TEST_CASE("Testing mostCalories") {
+  const std::string input = openTestFile(testFilePath);
   CalorieParser calorieParser(input);
   CHECK(calorieParser.mostCalories() == 24000);
 }
 
 TEST_CASE("Test topThreeTotal") {
+  const std::string input = openTestFile(testFilePath);
   CalorieParser calorieParser(input);
   CHECK(calorieParser.topThreeTotal() == 45000);
 }
--- src/Code/RockPaperScissorsTest.cpp
+++ src/Code/RockPaperScissorsTest.cpp
@@ -1,10 +1,24 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "RockPaperScissors.hpp"
 #include "doctest.h"
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-const std::string input = "A Y\nB X\nC Z\n";
+const std::string testFilePath = "input/day/2/test";
+
+std::string openTestFile(const std::string &aPath) {
+  std::ifstream inputFile(aPath);
+  assert(inputFile.is_open());
+
+  std::stringstream buffer;
+  buffer << inputFile.rdbuf();
+  return buffer.str();
+}
 
 TEST_CASE("Test Total Score") {
+  const std::string input = openTestFile(testFilePath);
   RockPaperScissors rockPaperScissors(input);
   CHECK(rockPaperScissors.getTotalScore() == 15);
 }
